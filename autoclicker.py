93d6047cd44a03f96e0292e5877a7374a5a28a5c
Rohan <rohangazi30@gmail.com>
2023-08-27 15:22:43 +0100

    setting variables

--- autoclicker.py
+++ autoclicker.py
@@ -1,2 +1,11 @@
 import pyautogui
 import time
+
+# delay before starting in seconds
+initalDelay = 5
+
+# click interval in seconds
+clickInterval = 1
+
+# number of clicks to perform
+numbClicks = 10
